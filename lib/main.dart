5e9026c2425e380655c0b891c09cccf6dfff8baf
jestyal <natali.doroshenko14@gmail.com>
2022-07-13 15:33:32 +0500

    Added notifications settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,62 +8,81 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Notification Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Notifications"), centerTitle: true),
+        body: const NotificationApp(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  final String title;
+class NotificationApp extends StatefulWidget {
+  const NotificationApp({ Key? key }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _NotificationAppState createState() => _NotificationAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _NotificationAppState extends State<NotificationApp> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late FlutterLocalNotificationsPlugin localNotifications;
+
+  @override
+  void initState() {
+    super.initState();
+    //объект для Android настроек
+    var androidInitialize = const AndroidInitializationSettings('ic_launcher');
+    //объект для IOS настроек
+    var IOSInitialize = const IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,);
+    // общая инициализация
+    var initializationSettings = InitializationSettings(
+        android: androidInitialize, iOS: IOSInitialize);
+
+    //мы создаем локальное уведомление
+    localNotifications = FlutterLocalNotificationsPlugin();
+    localNotifications.initialize(initializationSettings);
+  }
+
+  Future _showNotification() async {
+    var androidDetails = const AndroidNotificationDetails(
+      "ID",
+      "Название уведомления",
+      importance: Importance.high,
+      channelDescription: "Контент уведомления",
+    );
+
+    var iosDetails = const IOSNotificationDetails();
+    var generalNotificationDetails =
+    NotificationDetails(android: androidDetails, iOS: iosDetails);
+    await localNotifications.show(
+        0, "Название", "Тело уведомления", generalNotificationDetails);
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+
+      body: const Center(
+        child: Text('Нажми на кнопку, чтобы получить уведомление'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _showNotification,
+        child: const Icon(Icons.notifications),
       ),
+
     );
   }
 }
