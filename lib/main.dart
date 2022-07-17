8898749af17a93d586d821ae84b703adcaa05510
jestyal <natali.doroshenko14@gmail.com>
2022-07-17 15:34:49 +0500

    Added scheduled notifications

--- lib/main.dart
+++ lib/main.dart
@@ -55,9 +55,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   void initState() {
     super.initState();
+
+    _scheduleNotification();
   }
 
   @override
@@ -91,18 +94,9 @@ class _HomePageState extends State<HomePage> {
                       padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
                       child: ElevatedButton(
                         onPressed: () async {
-                          await _scheduleDailyTenAMNotification();
+                          await _scheduleNotification();
                         },
-                        child: const Text("Ежедневное уведомление в 10:00:00 в локальной таймзоне"),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await _checkPendingNotificationRequests();
-                        },
-                        child: const Text("Есть ли запланированные уведомления"),
+                        child: const Text("Запланированное уведомление"),
                       ),
                     ),
                     Padding(
@@ -114,17 +108,6 @@ class _HomePageState extends State<HomePage> {
                         child: const Text("Отменить все уведомления"),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await _zonedScheduleNotification();
-                        },
-                        child: const Text("Уведомление через 5 сек"),
-                      ),
-                    ),
-
-
                   ],
                 ),
               ),
@@ -133,78 +116,54 @@ class _HomePageState extends State<HomePage> {
         ),
       );
 
+  // простое уведомление по кнопке
   Future<void> _showNotification() async {
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
         AndroidNotificationDetails('ID', 'Название уведомления',
             channelDescription: 'Контент уведомления',
             importance: Importance.max, //Importance.high,
-            priority: Priority.high,
-            ticker: 'ticker');
-    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(0, "Привет", "Привет, мир!", platformChannelSpecifics);
+            priority: Priority.high);
+    const IOSNotificationDetails iosDetails = IOSNotificationDetails();
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iosDetails);
+    await flutterLocalNotificationsPlugin.show(
+        0,
+        "Уведомление",
+        "Простое уведомление", platformChannelSpecifics);
   }
 
-  Future<void> _zonedScheduleNotification() async {
+  // ежедневное напоминание в заданное время
+  Future<void> _scheduleNotification() async {
+    // задаем время напоминания в 24х-часовом формате
+    int hour = 15;
+    int minutes = 35;
+
     await flutterLocalNotificationsPlugin.zonedSchedule(
         0,
-        'Уведомление',
-        'через 5 секунд',
-        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+        'Ежедневное напоминание',
+        'Выделить полчаса на занятия программированием',
+        _nextInstanceOfTenAM(hour, minutes),
         const NotificationDetails(
             android: AndroidNotificationDetails(
                 'your channel id', 'your channel name',
                 channelDescription: 'your channel description')),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
-        UILocalNotificationDateInterpretation.absoluteTime);
-  }
-
-  Future<void> _checkPendingNotificationRequests() async {
-    final List<PendingNotificationRequest> pendingNotificationRequests =
-        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        content: Text('${pendingNotificationRequests.length} pending notification '
-            'requests'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _cancelAllNotifications() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
-  }
-
-  Future<void> _scheduleDailyTenAMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        'Ежедневное напоминание',
-        'Выделить полчаса на занятия программированием',
-        _nextInstanceOfTenAM(),
-        const NotificationDetails(
-          android: AndroidNotificationDetails('daily notification channel id', 'daily notification channel name',
-              channelDescription: 'daily notification description'),
-        ),
-        androidAllowWhileIdle: true,
-        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+        UILocalNotificationDateInterpretation.absoluteTime,
         matchDateTimeComponents: DateTimeComponents.time);
   }
 
-  tz.TZDateTime _nextInstanceOfTenAM() {
+  tz.TZDateTime _nextInstanceOfTenAM(int hour, int minutes) {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-    tz.TZDateTime scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, 14);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minutes);
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
     }
     return scheduledDate;
+  }
+
+  //отменить все напоминания
+  Future<void> _cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
   }
 
 }
